a5ebdec699f954209d23d4239a6173c930f9bee3
Denys G. Santos <gsdenys@gmai.com>
2021-11-26 23:21:35 -0300

    document the selector code

--- src/selector.lua
+++ src/selector.lua
@@ -80,26 +80,35 @@ function selector:isnginx() return selector.env == typename.NGINX end
 
 --- get socket based in the environment variable selection
 ---
---- @param envname any
---- @param fname any
---- @return any
+--- @param envname string the environment variable
+--- @param fname string the caller function name
+--- @return table - the table with the socket data
 local function from_env(envname, fname)
     local t = table({})
 
+    -- obtain the environment variable name and validate it
     envname = envname or defaultenv and defaultenv
     validate_env_name(envname, fname)
 
+    -- ibtain the envirinment variable value and validte it
+    -- case their value is not nil
     local envvalue = os.getenv(envname)
     if envvalue == nil then return t end
 
     validate_env_value(envvalue, fname)
 
+    -- set the env and socket data
     t.env = envvalue
     t.socket = types[envvalue]
 
     return t
 end
 
+--- obtain the nginx socket data and return true case there is
+--- some nginx socket.
+---
+---@param t any the table to update
+---@return boolean - true in case of it is nginx, other else false
 local function ngx(t)
     if _G.ngx and _G.ngx.socket then
         t.env = typename.NGINX
@@ -111,6 +120,11 @@ local function ngx(t)
     return false
 end
 
+--- obtain the cqueues socket data and return true case there is
+--- cqueues installed.
+---
+---@param t any the table to update
+---@return boolean - true in case of it's cqueues socket, other else false
 local function cqueues(t)
     local cq, _ = pcall(require, "cqueues")
 
@@ -124,26 +138,42 @@ local function cqueues(t)
     return false
 end
 
+--- discovery wich kind of socket should be used based on the execution
+--- environment and the module instaled on.
+---
+---@return any
 local function from_discovery()
     local t = table({})
 
+    -- test if there is nginx socket in the environment. In a positive
+    -- case just retun the actualized table
     if ngx(t) then return t end
 
+    -- test if there is cqueues socket in the environment. In a positive
+    -- case just retun the actualized table
     if cqueues(t) then return t end
 
+    -- case none of the above test fail, use the default lua socket
     t.env = typename.LUA
     t.socket = types.LUA
 
     return t
 end
 
+--- default builde to create socket selector base object
+---
+--- @param envname string the name of the used environment variable
+--- @return any - the socket selector
 local function builder(envname)
     local fname = "builder"
 
+    -- try to get soket based on env variable
     local t = from_env(envname, fname)
 
+    -- case has no environmet socket set, them start discovery
     if t.env ~= nil and t.socket ~= nil then t = from_discovery() end
 
+    -- add the selected docket data to this table
     selector:merge(t)
 
     return selector
